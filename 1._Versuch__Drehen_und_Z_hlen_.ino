b13c9dcf0b83c25c3d8729c35880911892dda2f9
TRauschendorfer <41699762+TRauschendorfer@users.noreply.github.com>
2018-07-26 11:11:54 +0200

    Add files via upload

---
+++ 1._Versuch__Drehen_und_Z_hlen_.ino
@@ -0,0 +1,32 @@
+int SpR=1200;
+int DirX=A0; 
+int StepX=A3; 
+int DT = 10; 
+int a; 
+
+
+
+
+void setup() 
+{
+
+pinMode(DirX, OUTPUT); 
+pinMode(StepX, OUTPUT);
+
+  Serial.begin(115200); 
+  a = 1;
+}
+
+void loop() 
+{
+  while(a <= SpR)
+  {
+  digitalWrite(DirX, HIGH);
+  digitalWrite(StepX, HIGH); 
+  digitalWrite(StepX, LOW);
+  Serial.println(a, DEC); 
+  a = a + 1; 
+  delayMicroseconds(DT);
+  } 
+}
+
